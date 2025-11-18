11a28431ea0d259411b85caf1d79e0c18ebcd11b
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-19 00:21:39 +0530

    done

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ let time = document.querySelector('#line span')
 let percent = 0
 btn.addEventListener('click', function(){
     btn.style.pointerEvents = 'none'
-    let num = 40 + Math.floor(Math.random()*50)
+    let num = 30 + Math.floor(Math.random()*100)
     time.innerHTML = `${num/10}`
+    btn.style.backgroundColor = '#b5bbc0ff'
+    btn.innerHTML = 'Downloading...'
 
     let inn = setInterval(function(){
         percent++
         h1.innerHTML = percent + '%'
         inner.style.width = percent + '%'
-        btn.style.backgroundColor = '#b5bbc0ff'
-        btn.innerHTML = 'Downloading...'
     }, num)
 
     setTimeout(function(){
-        clearInterval(inn)
+        if (percent >= 100) { clearInterval(inn) }
         btn.innerHTML = 'Downloaded'
         btn.style.backgroundColor = '#74777aff'
     }, num*100)
